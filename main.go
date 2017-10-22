684964ed54dfdc05d5d2c9006c55b58b2cf5f0a5
wagoodman <wagoodman@gmail.com>
2017-10-22 07:25:36 -0400

    fixed stdout dependency

--- main.go
+++ main.go
@@ -38,6 +38,7 @@ const (
 )
 
 var (
+	purple                      = color.ColorFunc("magenta+h")
 	red                         = color.ColorFunc("red+h")
 	green                       = color.ColorFunc("green")
 	bold                        = color.ColorFunc("default+b")
@@ -62,6 +63,7 @@ type ActionDisplay struct {
 
 type ActionCommand struct {
 	Cmd        *exec.Cmd
+	Started    bool
 	Complete   bool
 	ReturnCode int
 }
@@ -142,62 +144,6 @@ func (conf *Config) getConfig() {
 
 }
 
-func readPipe(resultChan chan PipeIR, pipe io.ReadCloser) {
-	scanner := bufio.NewScanner(pipe)
-
-	for scanner.Scan() {
-		message := scanner.Text()
-		resultChan <- PipeIR{vtclean.Clean(message, false)}
-	}
-}
-
-// actually use a cmd
-func (action *Action) runCmd(resultChan chan CmdIR, waiter *sync.WaitGroup) {
-	waiter.Add(1)
-	resultChan <- CmdIR{action, StatusPending, "", false, -1}
-
-	var waitStatus syscall.WaitStatus
-	var returnCode int
-	stdoutPipe, _ := action.Command.Cmd.StdoutPipe()
-	stderrPipe, _ := action.Command.Cmd.StderrPipe()
-
-	stdoutChan := make(chan PipeIR)
-	stderrChan := make(chan PipeIR)
-
-	go readPipe(stdoutChan, stdoutPipe)
-	go readPipe(stderrChan, stderrPipe)
-
-	action.Command.Cmd.Start()
-
-	select {
-	case stdoutMsg := <-stdoutChan:
-		resultChan <- CmdIR{action, StatusRunning, stdoutMsg.message, false, -1}
-	case stderrMsg := <-stderrChan:
-		resultChan <- CmdIR{action, StatusRunning, stderrMsg.message, false, -1}
-	}
-
-	err := action.Command.Cmd.Wait()
-
-	if exitError, ok := err.(*exec.ExitError); ok {
-		waitStatus = exitError.Sys().(syscall.WaitStatus)
-	} else {
-		waitStatus = action.Command.Cmd.ProcessState.Sys().(syscall.WaitStatus)
-	}
-	returnCode = waitStatus.ExitStatus()
-
-	// note, this is a possible race condition: this thread may be modifying action
-	// while the main thread is reading these fields. Find a better way to do this.
-	// (without locking... just because...)
-
-	waiter.Done()
-
-	if returnCode == 0 {
-		resultChan <- CmdIR{action, StatusSuccess, "", true, returnCode}
-	} else {
-		resultChan <- CmdIR{action, StatusError, "", true, returnCode}
-	}
-}
-
 func (action *Action) getParallelActions() (actions []*Action) {
 
 	if action.CmdString != "" {
@@ -267,8 +213,62 @@ func (action *Action) display(curLine *int) {
 	*curLine++
 }
 
-// mimic a cmd and show valid output
-func (action *Action) process() {
+func readPipe(resultChan chan PipeIR, pipe io.ReadCloser) {
+	scanner := bufio.NewScanner(pipe)
+
+	for scanner.Scan() {
+		message := scanner.Text()
+		resultChan <- PipeIR{vtclean.Clean(message, false)}
+	}
+}
+
+func (action *Action) reportOutput(resultChan chan CmdIR, stdoutPipe io.ReadCloser, stderrPipe io.ReadCloser) {
+
+	stdoutChan := make(chan PipeIR)
+	stderrChan := make(chan PipeIR)
+
+	go readPipe(stdoutChan, stdoutPipe)
+	go readPipe(stderrChan, stderrPipe)
+
+	select {
+	case stdoutMsg := <-stdoutChan:
+		resultChan <- CmdIR{action, StatusRunning, stdoutMsg.message, false, -1}
+	case stderrMsg := <-stderrChan:
+		resultChan <- CmdIR{action, StatusRunning, stderrMsg.message, false, -1}
+	}
+}
+
+func (action *Action) runCmd(resultChan chan CmdIR, waiter *sync.WaitGroup) {
+	waiter.Add(1)
+	resultChan <- CmdIR{action, StatusRunning, "", false, -1}
+
+	stdoutPipe, _ := action.Command.Cmd.StdoutPipe()
+	stderrPipe, _ := action.Command.Cmd.StderrPipe()
+	go action.reportOutput(resultChan, stdoutPipe, stderrPipe)
+	action.Command.Cmd.Start()
+
+	var waitStatus syscall.WaitStatus
+	var returnCode int
+
+	err := action.Command.Cmd.Wait()
+
+	if exitError, ok := err.(*exec.ExitError); ok {
+		waitStatus = exitError.Sys().(syscall.WaitStatus)
+	} else {
+		waitStatus = action.Command.Cmd.ProcessState.Sys().(syscall.WaitStatus)
+	}
+	returnCode = waitStatus.ExitStatus()
+
+	waiter.Done()
+
+	if returnCode == 0 {
+		resultChan <- CmdIR{action, StatusSuccess, "", true, returnCode}
+	} else {
+		resultChan <- CmdIR{action, StatusError, "", true, returnCode}
+	}
+}
+
+func (action *Action) process(step, totalTasks int) {
 
 	var (
 		curLine           int
@@ -283,21 +283,24 @@ func (action *Action) process() {
 
 	// make room for the title of a parallel proc group
 	if len(actions) > 1 {
-		lineObj := Line{StatusPending, bold(action.Name), "\n", ""}
+		lineObj := Line{StatusRunning, bold(action.Name), "\n", ""}
 		action.Display.Template.Execute(os.Stdout, lineObj)
 	}
 
 	for line := 0; line < len(actions); line++ {
-		actions[line].Display.Line = Line{StatusPending, actions[line].Name, "initializing...", ""}
+		actions[line].Command.Started = false
+		actions[line].Display.Line = Line{StatusPending, actions[line].Name, "", ""}
 		actions[line].display(&curLine)
 	}
 
 	var runningCmds int
 	for ; lastStartedAction < MaxParallelCmds && lastStartedAction < len(actions); lastStartedAction++ {
 		go actions[lastStartedAction].runCmd(resultChan, &action.waiter)
+		actions[lastStartedAction].Command.Started = true
 		runningCmds++
 	}
 	groupSuccess := StatusSuccess
+
 	// just wait for stuff to come back
 	for runningCmds > 0 {
 		select {
@@ -305,7 +308,7 @@ func (action *Action) process() {
 			spinner.Next()
 
 			for _, actionObj := range actions {
-				if actionObj.Command.Complete {
+				if actionObj.Command.Complete || !actionObj.Command.Started {
 					actionObj.Display.Line.Spinner = ""
 				} else {
 					actionObj.Display.Line.Spinner = spinner.Current()
@@ -325,6 +328,7 @@ func (action *Action) process() {
 				// if a thread has freed up, start the next action (if there are any left)
 				if lastStartedAction < len(actions) {
 					go actions[lastStartedAction].runCmd(resultChan, &action.waiter)
+					actions[lastStartedAction].Command.Started = true
 					runningCmds++
 					lastStartedAction++
 				}
@@ -383,7 +387,7 @@ func main() {
 
 	fmt.Print("\033[?25l") // hide cursor
 	for index := range conf.Tasks {
-		conf.Tasks[index].process()
+		conf.Tasks[index].process(index, len(conf.Tasks))
 	}
 	fmt.Print("\033[?25h") // show cursor
 
