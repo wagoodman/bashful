6c4df04c74a00c4c221f9df3fc14897928c56b19
Alex Goodman <wagoodman@gmail.com>
2017-11-15 16:10:37 -0500

    segfault example

---
+++ example/bin/download-oracle.sh
@@ -0,0 +1,22 @@
+#/usr/bin/env bash
+set -eux
+
+ORACLE_FILES=(instantclient-basiclite-linux.x64-12.1.0.2.0.zip instantclient-sqlplus-linux.x64-12.1.0.2.0.zip instantclient-sdk-linux.x64-12.1.0.2.0.zip)
+
+if [[ -d oracle ]]; then
+    mkdir -p downloads/oracle 
+    mv oracle/* downloads/oracle/
+    rm -rf oracle
+elif [[ ! -d downloads/oracle ]]; then
+    mkdir -p downloads/oracle 
+fi
+pushd downloads/oracle > /dev/null
+    for FILE in ${ORACLE_FILES[@]}; do
+        if [[ ! -e $FILE ]]; then
+        echo "Downloading $FILE"
+        curl --location "https://github.com/wagoodman/docker-ruby-node-oracle/raw/master/oracle/$FILE" -o $FILE
+        else
+        echo "Skipping download $FILE"
+        fi
+    done
+popd > /dev/null
