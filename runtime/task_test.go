c75be655cd8601848cce6c5e286c9824c64783dc
Alex Goodman <wagoodman@gmail.com>
2018-11-26 16:35:00 -0500

    improved task coverage

--- runtime/task_test.go
+++ runtime/task_test.go
@@ -1,6 +1,7 @@
 package runtime
 
 import (
+	"github.com/lunixbochs/vtclean"
 	"github.com/wagoodman/bashful/config"
 	"strings"
 	"testing"
@@ -213,7 +214,7 @@ func Test_Task_Execute(t *testing.T) {
 		expectedEvents []TaskEvent
 		expectedEnv    map[string]string
 	}{
-		"single task": {
+		"single task (success)": {
 			taskConfig: config.TaskConfig{
 				Name:      "easy task",
 				CmdString: "true",
@@ -224,13 +225,79 @@ func Test_Task_Execute(t *testing.T) {
 			},
 			expectedEnv: map[string]string{},
 		},
+		"single task (failure)": {
+			taskConfig: config.TaskConfig{
+				Name:      "easy task",
+				CmdString: "false",
+			},
+			expectedEvents: []TaskEvent{
+				{nil, StatusRunning, "", "", false, -1},
+				{nil, StatusError, "", "", true, 1},
+			},
+			expectedEnv: map[string]string{},
+		},
+		"single task (ignore failure)": {
+			taskConfig: config.TaskConfig{
+				Name:          "easy task",
+				CmdString:     "false",
+				IgnoreFailure: true,
+			},
+			expectedEvents: []TaskEvent{
+				{nil, StatusRunning, "", "", false, -1},
+				{nil, StatusSuccess, "", "", true, 1},
+			},
+			expectedEnv: map[string]string{},
+		},
+		"single task (env persist)": {
+			taskConfig: config.TaskConfig{
+				Name:          "easy task",
+				CmdString:     "export ANSWER=42",
+				IgnoreFailure: true,
+			},
+			expectedEvents: []TaskEvent{
+				{nil, StatusRunning, "", "", false, -1},
+				{nil, StatusSuccess, "", "", true, 0},
+			},
+			expectedEnv: map[string]string{
+				"INITIAL_TEST_DATA": "ALSO42",
+				"ANSWER":            "42",
+			},
+		},
+		"single task (stdout)": {
+			taskConfig: config.TaskConfig{
+				Name:        "easy task",
+				CmdString:   "echo sup",
+				EventDriven: true,
+			},
+			expectedEvents: []TaskEvent{
+				{nil, StatusRunning, "", "", false, -1},
+				{nil, StatusRunning, "sup", "", false, -1},
+				{nil, StatusSuccess, "", "", true, 0},
+			},
+			expectedEnv: map[string]string{},
+		},
+		"single task (stderr)": {
+			taskConfig: config.TaskConfig{
+				Name:        "easy task",
+				CmdString:   "echo meh >>/dev/stderr",
+				EventDriven: true,
+			},
+			expectedEvents: []TaskEvent{
+				{nil, StatusRunning, "", "", false, -1},
+				{nil, StatusRunning, "", "meh", false, -1},
+				{nil, StatusSuccess, "", "", true, 0},
+			},
+			expectedEnv: map[string]string{},
+		},
 	}
 
 	for name, testCase := range table {
-		t.Logf("Running test case: %s", name)
+		t.Logf("--- Running test case: %s", name)
 		task := NewTask(testCase.taskConfig, nil)
 
-		environment := make(map[string]string, 0)
+		environment := map[string]string{
+			"INITIAL_TEST_DATA": "ALSO42",
+		}
 
 		events := make([]TaskEvent, 0)
 		go task.Execute(task.events, &task.waiter, environment)
@@ -248,241 +315,44 @@ func Test_Task_Execute(t *testing.T) {
 		}
 
 		for idx, expEvent := range testCase.expectedEvents {
-			t.Logf("checking event %d...", idx)
 			actualEvent := events[idx]
 			if expEvent.Status != actualEvent.Status {
-				t.Errorf("   expected status=%v, got %v", expEvent.Status, actualEvent.Status)
+				t.Errorf("   event %d: expected status=%v, got %v", idx, expEvent.Status, actualEvent.Status)
 			}
 			if expEvent.Complete != actualEvent.Complete {
-				t.Errorf("   expected complete=%v, got %v", expEvent.Complete, actualEvent.Complete)
+				t.Errorf("   event %d: expected complete=%v, got %v", idx, expEvent.Complete, actualEvent.Complete)
 			}
 			if expEvent.ReturnCode != actualEvent.ReturnCode {
-				t.Errorf("   expected rc=%v, got %v", expEvent.ReturnCode, actualEvent.ReturnCode)
+				t.Errorf("   event %d: expected rc=%v, got %v", idx, expEvent.ReturnCode, actualEvent.ReturnCode)
 			}
-			if expEvent.Stderr != actualEvent.Stderr {
-				t.Errorf("   expected stderr='%v', got '%v'", expEvent.Stderr, actualEvent.Stderr)
+			if expEvent.Stderr != vtclean.Clean(actualEvent.Stderr, false) {
+				t.Errorf("   event %d: expected stderr='%v', got '%v'", idx, expEvent.Stderr, actualEvent.Stderr)
 			}
-			if expEvent.Stdout != actualEvent.Stdout {
-				t.Errorf("   expected stdout='%v', got '%v'", expEvent.Stdout, actualEvent.Stdout)
+			if expEvent.Stdout != vtclean.Clean(actualEvent.Stdout, false) {
+				t.Errorf("   event %d: expected stdout='%v', got '%v'", idx, expEvent.Stdout, actualEvent.Stdout)
+			}
+
+		}
+
+		// this is not valid, there may be several variables generated, few of which are intentional
+		// if len(environment) != len(testCase.expectedEnv) {
+		// 	for key, actualValue := range environment {
+		// 		t.Errorf("   environment[%s] = %s", key, actualValue)
+		// 	}
+		// 	t.Fatalf("expected %v environment variables, got %v", len(testCase.expectedEnv), len(environment))
+		// }
+
+		for key, expValue := range testCase.expectedEnv {
+
+			if actualValue, exists := environment[key]; exists {
+				if expValue != actualValue {
+					t.Errorf("   expected env[%s]=%v, got %v", key, expValue, actualValue)
+				}
+			} else {
+				t.Errorf("   missing environment key '%s'", key)
 			}
 
 		}
 
 	}
 }
-
-//
-// func TestSerialTaskEnvPersistence(t *testing.T) {
-// 	var expStr, actStr string
-// 	var failedTasks []*Task
-// 	simpleYamlStr := `
-// tasks:
-//   - name: startNextSubTasks
-//     cmd: export SOMEVAR=this
-//
-//   - name: append 'is'
-//     cmd: export SOMEVAR=$SOMEVAR:is
-//
-//   - name: append 'DONTDOIT'
-//     parallel-Tasks:
-//       - cmd: export SOMEVAR=$SOMEVAR:DONTDOIT
-//
-//   - name: append '<replace>'
-//     cmd: export SOMEVAR=$SOMEVAR:<replace>
-//     for-each:
-//       - working
-//       - just
-//
-//   - name: append 'is'
-//     cmd: eval 'export SOMEVAR=$SOMEVAR:fine'
-// `
-//
-// 	environment := map[string]string{}
-// 	config.Config.Options.StopOnFailure = false
-// 	failedTasks = Execute([]byte(simpleYamlStr), environment)
-// 	if len(failedTasks) > 0 {
-// 		t.Error("TestSerialTaskEnvPersistence: Expected no Tasks to fail")
-// 	}
-//
-// 	expStr, actStr = "this:is:working:just:fine", environment["SOMEVAR"]
-// 	if expStr != actStr {
-// 		t.Error("Expected", expStr, "got", actStr)
-// 	}
-//
-// }
-
-//
-// func TestCurrentWorkingDirectory(t *testing.T) {
-// 	var expStr, actStr string
-// 	var failedTasks []*Task
-// 	simpleYamlStr := `
-// Tasks:
-//   - name: startNextSubTasks
-//     cmd: export CWD=$(pwd)
-//     cwd: ../example
-// `
-//
-// 	environment := map[string]string{}
-// 	config.Config.Options.StopOnFailure = false
-// 	failedTasks = Execute([]byte(simpleYamlStr), environment)
-// 	if len(failedTasks) > 0 {
-// 		t.Error("TestSerialTaskEnvPersistence: Expected no Tasks to fail")
-// 	}
-// 	cwd := strings.Split(environment["CWD"], "/")
-// 	expStr, actStr = "example", cwd[len(cwd)-1]
-// 	if expStr != actStr {
-// 		t.Error("Expected", expStr, "got", actStr)
-// 	}
-//
-// }
-//
-// func TestCommandArguments(t *testing.T) {
-// 	yamlStr := `
-// Tasks:
-//   - cmd: command-with-args $1 $2
-//     name: Arg1=$1 Arg2=$2
-//   - cmd: all-args $*
-//     name: Args=$*
-// `
-//
-// 	config.Config.Cli.Args = []string{"First", "Second"}
-// 	config.parseRunYaml([]byte(yamlStr))
-// 	tasks := CreateTasks()
-// 	if len(tasks) != 2 {
-// 		t.Error("Expected two Tasks. Got: ", len(tasks))
-// 	}
-//
-// 	if tasks[0].Config.CmdString != "command-with-args First Second" {
-// 		t.Error("Expected arguments to be replaced. Got: ", tasks[0].Config.CmdString)
-// 	}
-// 	if tasks[0].Config.Name != "Arg1=First Arg2=Second" {
-// 		t.Error("Expected arguments to be replaced in task name. Got: ", tasks[0].Config.Name)
-// 	}
-//
-// 	if tasks[1].Config.CmdString != "all-args First Second" {
-// 		t.Error("Expected all arguments to be replaced. Got: ", tasks[1].Config.CmdString)
-// 	}
-// 	if tasks[1].Config.Name != "Args=First Second" {
-// 		t.Error("Expected all arguments to be replaced in task name. Got: ", tasks[1].Config.Name)
-// 	}
-// }
-//
-// func TestCreateTasks_SuccessfulParse(t *testing.T) {
-// 	var expStr, actStr string
-// 	var expOpt, actOpt bool
-// 	var exNum int
-// 	simpleYamlStr := `
-// Tasks:
-//   - cmd: random-worker.sh 10
-//   - name: Compiling source
-//     parallel-Tasks:
-//       - cmd: compile-something.sh 2
-//       - cmd: compile-something.sh 9
-//         stop-on-failure: false
-//         show-output: false
-//       - cmd: compile-something.sh 6
-//       - cmd: compile-something.sh 4 <replace>
-//         for-each:
-//           - plug 4
-//           - plug 5
-//           - plug 6
-//   - name: some random task name <replace>
-//     cmd: random-worker.sh 2
-//     for-each:
-//       - plug 3
-//   - cmd: random-worker.sh 10 <replace>
-//     for-each:
-//       - plug 1
-//       - plug 2
-// `
-// 	// load test time cache
-// 	config.Config.cmdEtaCache = make(map[string]time.Duration)
-// 	config.Config.cmdEtaCache["compile-something.sh 2"] = time.Duration(2 * time.Second)
-// 	config.Config.cmdEtaCache["compile-something.sh 4"] = time.Duration(4 * time.Second)
-// 	config.Config.cmdEtaCache["compile-something.sh 6"] = time.Duration(6 * time.Second)
-// 	config.Config.cmdEtaCache["compile-something.sh 9"] = time.Duration(9 * time.Second)
-// 	config.Config.cmdEtaCache["compile-something.sh 10"] = time.Duration(10 * time.Second)
-//
-// 	// load test Config yaml
-// 	config.parseRunYaml([]byte(simpleYamlStr))
-// 	// create and compile Tasks
-// 	tasks := CreateTasks()
-//
-// 	// validate test task yaml
-//
-// 	exNum = 5
-// 	if len(tasks) != exNum {
-// 		t.Error("Expected", exNum, "Tasks got", len(tasks))
-// 	}
-//
-// 	exNum = 6
-// 	if len(tasks[1].Children) != exNum {
-// 		t.Error("Expected", exNum, "parallel Tasks got", len(tasks[1].Children))
-// 	}
-//
-// 	// ensure that names are set properly
-//
-// 	expStr, actStr = "Compiling source", tasks[1].Config.Name
-// 	if actStr != expStr {
-// 		t.Error("Expected name:", expStr, "got name:", actStr)
-// 	}
-//
-// 	expStr, actStr = "some random task name plug 3", tasks[2].Config.Name
-// 	if actStr != expStr {
-// 		t.Error("Expected name:", expStr, "got name:", actStr)
-// 	}
-//
-// 	// check the names of the top task list
-//
-// 	for _, taskIndex := range []int{0, 3, 4} {
-// 		repr.Println(tasks[taskIndex].Config)
-// 		expStr, actStr = tasks[taskIndex].Config.CmdString, tasks[taskIndex].Config.Name
-// 		if actStr != expStr {
-// 			t.Error("Expected name:", expStr, "got name:", actStr)
-// 		}
-// 	}
-//
-// 	// check the names of the top parallel list
-// 	for taskIndex := 0; taskIndex < len(tasks[1].Children); taskIndex++ {
-// 		expStr, actStr = tasks[1].Children[taskIndex].Config.CmdString, tasks[1].Children[taskIndex].Config.Name
-// 		if actStr != expStr {
-// 			t.Error("Expected name:", expStr, "got name:", actStr)
-// 		}
-// 	}
-//
-// 	// ensure that names and commands of for-each Tasks fill in the ? character with params
-// 	for _, taskIndex := range []int{3, 4} {
-// 		expStr, actStr = tasks[taskIndex].Config.CmdString, tasks[taskIndex].Config.Name
-// 		if actStr != expStr {
-// 			t.Error("Expected name:", expStr, "got name:", actStr)
-// 		}
-// 		if !strings.Contains(tasks[taskIndex].Config.Name, "plug") {
-// 			t.Error("Expected name to contain 'plug' but got:", actStr)
-// 		}
-// 		if !strings.Contains(tasks[taskIndex].Config.CmdString, "plug") {
-// 			t.Error("Expected cmd to contain 'plug' but got:", actStr)
-// 		}
-// 	}
-//
-// 	expStr, actStr = "some random task name plug 3", tasks[2].Config.Name
-// 	if actStr != expStr {
-// 		t.Error("Expected name:", expStr, "got name:", actStr)
-// 	}
-//
-// 	expStr, actStr = "random-worker.sh 2", tasks[2].Config.CmdString
-// 	if actStr != expStr {
-// 		t.Error("Expected cmd:", expStr, "got cmd:", actStr)
-// 	}
-//
-// 	// ensure stop on fail and show output can be overridden to false
-// 	expOpt, actOpt = false, tasks[1].Children[1].Config.ShowTaskOutput
-// 	if actOpt != expOpt {
-// 		t.Error("Expected name:", expOpt, "got name:", actOpt)
-// 	}
-//
-// 	expOpt, actOpt = false, tasks[1].Children[1].Config.StopOnFailure
-// 	if actOpt != expOpt {
-// 		t.Error("Expected name:", expOpt, "got name:", actOpt)
-// 	}
-//
-// }
